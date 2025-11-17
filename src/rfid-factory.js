085be4c3c3e6ec7a7a4feeea80e798ce3b977c97
Lee <846046245@qq.com>
2025-11-17 13:40:37 +0800

    init

--- src/rfid-factory.js
+++ src/rfid-factory.js
@@ -2,11 +2,16 @@ import { RfidWYuan } from "./models/rfid-w-yuan";
 import { RfidImpinj } from "./models/rfid-impinj";
 
 export class RfidFactory {
+  static MODE = {
+    w_yuan: "w-yuan",
+    impinj: "impinj",
+  };
+
   static createInterface(type) {
     switch (type.toLowerCase()) {
-      case "w-yuan":
+      case RfidFactory.MODE.w_yuan:
         return new RfidWYuan();
-      case "impinj":
+      case RfidFactory.MODE.impinj:
         return new RfidImpinj();
       default:
         throw new Error(`未知的RFID接口类型: ${type}`);
